b9a724049cad897fc3623ac5d23b53c7ccab10c0
Nikesh <nikesh123u@gmail.com>
2022-02-06 10:01:39 +0545

    Player turn

--- client/src/client.js
+++ client/src/client.js
@@ -7,20 +7,48 @@ const log = (text) => {
   parent.scrollTop = parent.scrollHeight;
 };
 
-const onChatSubmitted = (e) => {
+const onChatSubmitted = (sock) => (e) => {
   e.preventDefault();
 
   const input = document.querySelector("#chat");
   const text = input.value;
   input.value = "";
+  sock.emit("message", text);
+};
 
-  log(text);
+const getBoard = (canvas) => {
+  const ctx = canvas.getContext("2d");
+  const fillRect = (x, y, color) => {
+    ctx.fillStyle = color;
+    ctx.fillRect(x - 10, y - 10, 20, 20);
+  };
+  return { fillRect };
+};
+
+const getClickCoordinates = (element, ev) => {
+  const { top, left } = element.getBoundingClientRect();
+  const { clientX, clientY } = ev;
+  return {
+    x: clientX - left,
+    y: clientY - top,
+  };
 };
 
 (() => {
-  log("chat is disabled on this server");
+  const canvas = document.querySelector("canvas");
+  const { fillRect } = getBoard(canvas);
+  fillRect(50, 50, "blue");
+  const sock = io();
+
+  const onClick = (e) => {
+    const { x, y } = getClickCoordinates(canvas, e);
+    sock.emit("turn", { x, y });
+  };
+  sock.on("message", log);
+  sock.on("turn", ({ x, y }) => fillRect(x, y));
 
   document
     .querySelector("#chat-form")
-    .addEventListener("submit", onChatSubmitted);
+    .addEventListener("submit", onChatSubmitted(sock));
+  canvas.addEventListener("click", onClick);
 })();
--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
 
 app.use(express.static(`${__dirname}/../client`)); // Directory for file location
 
 const server = http.createServer(app);
+const io = socketio(server); // It just works as a wrapper for server, every request which is related to socketio it will  filter out else all it will pass it on to server (Express Application)
+
+io.on("connection", (sock) => {
+  // Each time a person tries to connect we get new invocation of this instance.
+  sock.emit("message", "You are connected");
+  sock.on("message", (text) => io.emit("message", text));
+  sock.on("turn", ({ x, y }) => io.emit("turn", { x, y }));
+});
 
 server.on("error", (err) => {
   console.error(err);
